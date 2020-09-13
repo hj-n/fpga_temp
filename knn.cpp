2db5fe89c32e534dd03abe946b65764f556a45a6
Hyeon Jeon <hj@hcil.snu.ac.kr>
2020-09-13 15:42:44 +0900

    knn code revised

--- knn.cpp
+++ knn.cpp
@@ -51,29 +51,44 @@ void knn(
 		unsigned int* out,
 		int size,
 		int k,
+		int dim,
 		const unsigned int* v
 
         )
 {
-#pragma HLS INTERFACE m_axi port=in1  offset=slave bundle=gmem
-#pragma HLS INTERFACE m_axi port=in2  offset=slave bundle=gmem
-#pragma HLS INTERFACE m_axi port=out offset=slave bundle=gmem
-#pragma HLS INTERFACE s_axilite port=in1  bundle=control
-#pragma HLS INTERFACE s_axilite port=in2  bundle=control
-#pragma HLS INTERFACE s_axilite port=out bundle=control
-#pragma HLS INTERFACE s_axilite port=size bundle=control
-#pragma HLS INTERFACE s_axilite port=k bundle=control
-#pragma HLS INTERFACE s_axilite port=v1 bundle=control
-#pragma HLS INTERFACE s_axilite port=v2 bundle=control
-#pragma HLS INTERFACE s_axilite port=return bundle=control
+// #pragma HLS INTERFACE m_axi port=in1  offset=slave bundle=gmem
+// #pragma HLS INTERFACE m_axi port=in2  offset=slave bundle=gmem
+// #pragma HLS INTERFACE m_axi port=out offset=slave bundle=gmem
+// #pragma HLS INTERFACE s_axilite port=in1  bundle=control
+// #pragma HLS INTERFACE s_axilite port=in2  bundle=control
+// #pragma HLS INTERFACE s_axilite port=out bundle=control
+// #pragma HLS INTERFACE s_axilite port=size bundle=control
+// #pragma HLS INTERFACE s_axilite port=k bundle=control
+// #pragma HLS INTERFACE s_axilite port=v1 bundle=control
+// #pragma HLS INTERFACE s_axilite port=v2 bundle=control
+// #pragma HLS INTERFACE s_axilite port=return bundle=control
+
+	#pragma HLS INTERFACE m_axi port=in  offset=slave bundle=gmem
+	#pragma HLS INTERFACE m_axi port=out offset=slave bundle=gmem
+	#pragma HLS INTERFACE m_axi port=v   offset=slave bundle=gmem
+
+	#pragma HLS INTERFACE s_axilite port=in     bundle=control	
+	#pragma HLS INTERFACE s_axilite port=out    bundle=control
+	#pragma HLS INTERFACE s_axilite port=size   bundle=control
+	#pragma HLS INTERFACE s_axilite port=k      bundle=control
+	#pragma HLS INTERFACE s_axilite port=v      bundle=control
+	#pragma HLS INTERFACE s_axilite port=return bundle=control
 
 
-
-
-    unsigned int v1_buffer[BUFFER_SIZE];    // Local memory to store vector1
-    unsigned int v2_buffer[BUFFER_SIZE];    // Local memory to store vector2
-    unsigned int k_idx_buffer[BUFFER_OUTPUT_SIZE];  // Local Memory to store result
+    unsigned int in_buffer[BUFFER_SIZE][dim];	
+	unsigned int k_idx_buffer[BUFFER_OUTPUT_SIZE]; 
 	int k_dist_buffer[BUFFER_OUTPUT_SIZE];
+
+
+    // unsigned int v1_buffer[BUFFER_SIZE];    // Local memory to store vector1
+    // unsigned int v2_buffer[BUFFER_SIZE];    // Local memory to store vector2
+    // unsigned int k_idx_buffer[BUFFER_OUTPUT_SIZE];  // Local Memory to store result
+	// int k_dist_buffer[BUFFER_OUTPUT_SIZE];
 	
 	for (int i = 0 ; i < k; i++) {
 		k_dist_buffer[i] = 100000000;
@@ -85,18 +100,26 @@ void knn(
         if ((i + BUFFER_SIZE) > size) 
             chunk_size = size - i;
 
-        read1: for (int j = 0 ; j < chunk_size ; j++){
-            v1_buffer[j] = in1[i + j];
-        }
-        read2: for (int j = 0 ; j < chunk_size ; j++){
-            v2_buffer[j] = in2[i + j];
-        }
+        // read1: for (int j = 0 ; j < chunk_size ; j++){
+        //     in_buffer[j] = in[i + j];
+        // }
+        // read2: for (int j = 0 ; j < chunk_size ; j++){
+        //     v2_buffer[j] = in2[i + j];
+        // }
 
-		for (int j = 0; j < chunk_size ; j++) {
-			int x = v1_buffer[j];
-			int y = v2_buffer[j];
-			int dist = (v1 - x) * (v1 - x) + (v2 - y) * (v2 - y);
-			
+		read: for (int j = 0; j < chunk_size; j++) {
+			for(int m = 0; m < dim; m++) {
+				in_buffer[j][m] = in[i + j][m];
+			}
+		}
+
+		for (int j = 0; j < chunk_size; j++) {
+			int dist = 0;
+			for(int m = 0; m < dim; m++) {
+				int dj = in_buffer[j][m] - v[m];
+				dist += dj * dj;
+			}
+
 			for (int m = 0 ; m < k ; m++) {
 				if (k_dist_buffer[m] > dist) {
 					for (int n = k - 1 ; n > m ; n--) {
@@ -108,7 +131,27 @@ void knn(
 					break;
 				}
 			}
+
+
 		}
+
+		// for (int j = 0; j < chunk_size ; j++) {
+		// 	int x = v1_buffer[j];
+		// 	int y = v2_buffer[j];
+		// 	int dist = (v1 - x) * (v1 - x) + (v2 - y) * (v2 - y);
+			
+		// 	for (int m = 0 ; m < k ; m++) {
+		// 		if (k_dist_buffer[m] > dist) {
+		// 			for (int n = k - 1 ; n > m ; n--) {
+		// 				k_idx_buffer[n] = k_idx_buffer[n - 1];
+		// 				k_dist_buffer[n] = k_dist_buffer[n - 1];
+		// 			}
+		// 			k_idx_buffer[m] = i + j;
+		// 			k_dist_buffer[m] = dist;
+		// 			break;
+		// 		}
+		// 	}
+		// }
     }
 
 	write: for (int i = 0 ; i < k ; i++) {

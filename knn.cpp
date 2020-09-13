c70761cd86197aef817290e3d0a4f9ae1f2cdc64
Hyeon Jeon <hj@hcil.snu.ac.kr>
2020-09-13 17:58:18 +0900

    kernel code revert

--- knn.cpp
+++ knn.cpp
@@ -1,21 +1,17 @@
+
 /**********
 Copyright (c) 2018, Xilinx, Inc.
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
-
 1. Redistributions of source code must retain the above copyright notice,
 this list of conditions and the following disclaimer.
-
 2. Redistributions in binary form must reproduce the above copyright notice,
 this list of conditions and the following disclaimer in the documentation
 and/or other materials provided with the distribution.
-
 3. Neither the name of the copyright holder nor the names of its contributors
 may be used to endorse or promote products derived from this software
 without specific prior written permission.
-
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -109,14 +105,14 @@ void knn(
 
 		read: for (int j = 0; j < chunk_size; j++) {
 			for(int m = 0; m < dim; m++) {
-				in_buffer[j][m] = in[(i + j) * dim + m];
+				in_buffer[j][m] = in[i + j + m];
 			}
 		}
 
 		for (int j = 0; j < chunk_size; j++) {
 			int dist = 0;
 			for(int m = 0; m < dim; m++) {
-				int dj = v[m] - in_buffer[j][m];
+				int dj = in_buffer[j][m] - v[m];
 				dist += dj * dj;
 			}
 
